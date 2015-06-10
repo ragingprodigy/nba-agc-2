762aea9f7e352510c0cbc96a5fdd1266e3c31969
Omonayajo Oladapo Adeola <o.omonayajo@gmail.com>
2015-06-10 01:40:12 +0100

    Initial app commit

---
+++ client/app/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('nbaAgc2App', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router',
+  'ui.bootstrap'
+])
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
