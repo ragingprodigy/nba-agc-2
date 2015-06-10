d3b2313926cca770a2a13496878596648b1364da
Omonayajo Oladapo Adeola <o.omonayajo@gmail.com>
2015-06-10 02:20:17 +0100

    Secured the API with Stormpath

--- server/config/local.env.js
+++ server/config/local.env.js
@@ -6,8 +6,12 @@
 // You will need to set these on the server you deploy to.
 
 module.exports = {
-  DOMAIN: 'http://localhost:9000',
-  SESSION_SECRET: "nbaagc2-secret",
-  // Control debug level for modules using visionmedia/debug
-  DEBUG: ''
+    DOMAIN: 'http://localhost:9000',
+    SESSION_SECRET: "nbaagc2-se78683*&^*hg3j5cret",
+    // Control debug level for modules using visionmedia/debug
+    DEBUG: '',
+
+    STORMPATH_API_KEY_ID: '6GVSYRJZ6I4M7XYT8X0UKIJFA',
+    STORMPATH_API_KEY_SECRET: 'Yj+BTI3+YGITPI3aoRhBP6kuHOSJY1cVD17yiBbV1xM',
+    STORMPATH_APP_HREF: 'https://api.stormpath.com/v1/applications/5fmXKTra2WYanXs9gTiIB9'
 };
--- server/routes.js
+++ server/routes.js
@@ -6,10 +6,15 @@
 
 var errors = require('./components/errors');
 
+var stormpathExpressSdk = require('stormpath-sdk-express');
+var spMiddleware = stormpathExpressSdk.createMiddleware();
+
 module.exports = function(app) {
 
+    spMiddleware.attachDefaults(app);
+
   // Insert routes below
-  app.use('/api/things', require('./api/thing'));
+  app.use('/api/things', spMiddleware.authenticate, require('./api/thing'));
   
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
