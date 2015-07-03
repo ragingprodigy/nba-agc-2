ee419b0afe471d6e17b12e24ca0954545dc14280
Omonayajo Oladapo Adeola <o.omonayajo@gmail.com>
2015-07-03 11:33:59 +0100

    Removed support for concurrency

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ var mongoose = require('mongoose');
 var config = require('./config/environment');
 var rollbar = require('rollbar');
 
-var cluster = require('cluster');
+/*var cluster = require('cluster');
 
 if (cluster.isMaster) {
 
@@ -57,4 +57,27 @@ if (cluster.isMaster) {
 
 	// Expose app
 	exports = module.exports = app;
-}
+}*/
+// Connect to database
+mongoose.connect(config.mongo.uri, config.mongo.options);
+
+// Populate DB with sample data
+if(config.seedDB) { require('./config/seed'); }
+
+// Setup server
+var app = express();
+var server = require('http').createServer(app);
+require('./config/express')(app);
+require('./routes')(app);
+require('./backgroundTasks').start();
+
+// Use the rollbar error handler to send exceptions to your rollbar account
+app.use(rollbar.errorHandler('fd9dcb150036419292ea7ae4bd8b4e0e'));
+
+// Start server
+server.listen(config.port, config.ip, function () {
+  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+});
+
+// Expose app
+exports = module.exports = app;
