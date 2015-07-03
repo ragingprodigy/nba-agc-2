d486974f6919ced6d5c6404e5fdece0b550a9334
Omonayajo Oladapo Adeola <o.omonayajo@gmail.com>
2015-07-03 01:47:20 +0100

    Added support for concurrency

--- server/app.js
+++ server/app.js
@@ -12,26 +12,49 @@ var mongoose = require('mongoose');
 var config = require('./config/environment');
 var rollbar = require('rollbar');
 
-// Connect to database
-mongoose.connect(config.mongo.uri, config.mongo.options);
+var cluster = require('cluster');
 
-// Populate DB with sample data
-if(config.seedDB) { require('./config/seed'); }
+if (cluster.isMaster) {
 
-// Setup server
-var app = express();
-var server = require('http').createServer(app);
-require('./config/express')(app);
-require('./routes')(app);
-require('./backgroundTasks').start();
+    // Count the machine's CPUs
+    var cpuCount = require('os').cpus().length;
+    console.log('There are '+cpuCount+ ' CPUs here!!!!');
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
+    // Listen for dying workers
+	cluster.on('exit', function (worker) {
+	    // Replace the dead worker, we're not sentimental
+	    console.log('Worker ' + worker.id + ' died :(');
+	    cluster.fork();
+	});
 
-// Use the rollbar error handler to send exceptions to your rollbar account
-app.use(rollbar.errorHandler('fd9dcb150036419292ea7ae4bd8b4e0e'));
+// Code to run if we're in a worker process
+} else {
 
-// Start server
-server.listen(config.port, config.ip, function () {
-  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
-});
+	// Connect to database
+	mongoose.connect(config.mongo.uri, config.mongo.options);
 
-// Expose app
-exports = module.exports = app;
+	// Populate DB with sample data
+	if(config.seedDB) { require('./config/seed'); }
+
+	// Setup server
+	var app = express();
+	var server = require('http').createServer(app);
+	require('./config/express')(app);
+	require('./routes')(app);
+	require('./backgroundTasks').start();
+
+	// Use the rollbar error handler to send exceptions to your rollbar account
+	app.use(rollbar.errorHandler('fd9dcb150036419292ea7ae4bd8b4e0e'));
+
+	// Start server
+	server.listen(config.port, config.ip, function () {
+	  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+	  console.log('Worker ' + cluster.worker.id + ' running!');
+	});
+
+	// Expose app
+	exports = module.exports = app;
+}
