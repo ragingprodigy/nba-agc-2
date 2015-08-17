6b51ead668f4a6e1d1a3e885dfa9f0df9554c71c
Omonayajo Oladapo Adeola <o.omonayajo@gmail.com>
2015-08-17 15:05:07 +0100

    Reduce speakers response payload for the web app.

--- client/app/conference_speakers/conference_speakers.controller.js
+++ client/app/conference_speakers/conference_speakers.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('nbaAgc2App')
 .controller('ConferenceSpeakersCtrl', function ($scope, Speakers, $state, Utils) {
-    $scope.speakers = Speakers.query({});
+    $scope.speakers = Speakers.query({lean:true});
 
     $scope.showSpeakerDetail = function(id, name){
           $state.go('speaker_detail', {id:id, name: Utils.slug(name)});
