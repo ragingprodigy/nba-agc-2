e73359d4cc3328021c5b2c0ebb876bfce2d29b12
Omonayajo Oladapo Adeola <o.omonayajo@gmail.com>
2015-08-17 14:28:52 +0100

    Temporarily disable Listing of Conference Attendees

--- client/app/attendees/attendees.controller.js
+++ client/app/attendees/attendees.controller.js
@@ -3,12 +3,12 @@
 angular.module('nbaAgc2App')
 .controller('AttendeesCtrl', function ($scope, Registration, $timeout) {
 
-    Registration.attendees({}).$promise.then(function(registrations){
+    /*Registration.attendees({}).$promise.then(function(registrations){
         $scope.registrations = registrations;
 
         $timeout(function(){
             angular.element('.dataTables_filter input, .dataTables_length select').addClass('form-control');
         }, 1000);
-    });
+    });*/
 
 });
