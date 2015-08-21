dc96c948fc14da86ddcfb1c40bd0fb53606bb722
Omonayajo Oladapo Adeola <o.omonayajo@gmail.com>
2015-08-21 01:55:22 +0100

    Disable access to Papers for Guests

--- client/app/papers/papers.controller.js
+++ client/app/papers/papers.controller.js
@@ -1,7 +1,9 @@
 'use strict';
 
 angular.module('nbaAgc2App')
-  .controller('PapersCtrl', function ($scope, Sessions) {
+  .controller('PapersCtrl', function ($scope, Sessions, $auth, $state) {
+
+        if (!$auth.isAuthenticated()) { $state.go('conference_sessions'); }
 
         Sessions.papers({}, function(papers){
             $scope.papers = papers;
