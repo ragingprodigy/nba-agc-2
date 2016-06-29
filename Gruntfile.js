afe72d8387f7b6ce904d4d08f0d211597d284b2c
Omonayajo Oladapo <o.omonayajo@gmail.com>
2016-06-29 13:19:54 +0100

    Updated copy task for Procfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -346,7 +346,8 @@ module.exports = function (grunt) {
             'bower_components/**/*',
             'assets/images/{,*/}*.{webp}',
             'assets/fonts/**/*',
-            'index.html'
+            'index.html',
+            'Procfile'
           ]
         }, {
           expand: true,
